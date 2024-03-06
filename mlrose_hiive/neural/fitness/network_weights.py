f707f1c726f73c469db8b9f63df1f45e9895e527
Jason Raubenheimer <raubenheimer.jason@gmail.com>
2024-03-06 18:32:49 +0000

    back to maximization

--- mlrose_hiive/neural/fitness/network_weights.py
+++ mlrose_hiive/neural/fitness/network_weights.py
@@ -145,8 +145,7 @@ class NetworkWeights:
                 self.y_pred = self.output_activation(outputs)
 
         # Evaluate loss function
-        # fitness = -1 * self.loss(self.y_true, self.y_pred)
-        fitness = self.loss(self.y_true, self.y_pred)
+        fitness = -1 * self.loss(self.y_true, self.y_pred)
 
         return fitness
 
